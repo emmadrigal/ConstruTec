619ab66b008d60146c968fa84213d01cb0d14cba
Emmanuel Madrigal <emma.madri97@gmail.com>
2016-10-06 18:50:10 -0600

    Added population for clients and engineers & added a trigger

--- Creation.sql
+++ Creation.sql
@@ -8,7 +8,7 @@ Phone_Number integer
 
 CREATE TABLE ENGINEER(
 Id_Number    bigint      PRIMARY KEY,
-Code         integer     NOT NULL,
+Code         varchar(50) NOT NULL,
 Name         varchar(50) NOT NULL,
 Phone_Number integer
 );
@@ -55,5 +55,24 @@ Id_Material bigint    REFERENCES MATERIAL(Id_Material),
 Stage_Id    bigint    REFERENCES STAGE(Stage_Id),
 Quantity    integer   NOT NULL CHECK (Quantity > 0)
 );
+
+INSERT INTO STAGE (Stage_Id,Name,Description) VALUES (0, 'Trabajo Preliminar', 'Consiste de solicitud de permisos y otras labores administrativas'), (1, 'Cimientos', ' Elementos estructurales encargados de transmitir el peso del edificio al suelo'), (2, 'Paredes', 'Divide distintas partes de la edificación y provee soporte al techo'), (3, 'Concreto Reforzado', 'Consiste en utilizar varillas para darle soporte al muro'), (4, 'Techos', 'Protege al edificio de lluvia y otros problemas atmosféricos'), (5, 'Cielos', 'Soporte para diferentes estructuras en el techo'), (6, 'Repello', 'Cubierta para la pared'), (7, 'Entrepisos', 'Piso entre los distintos niveles de una edificación'), (8, 'Pisos', 'Cubierta sobre el suelo'), (9, 'Enchapes', 'Relleno para las grietas en el piso'), (10, 'Instalación Pluvial', 'Utilizado para evacuar aguas provenientes de la lluvia'), (11, 'Instalación Sanitaria', 'Evacuación de las aguas negras generadas en la casa'), (12, 'Puertas', 'Provee acceso a los distintos cuartos'), (13, 'Cerrajería', 'Permite mayor seguridad'), (14, 'Ventanas', 'Permite ver afuera de la casa'), (15, 'Closets', 'Utilizado para guardar vestimenta'), (16, 'Mueble de Cocina', 'Utilizado para guardar comida'), (17, 'Escaleras', '');
+
+--Protects from deletions on the default values of the stage table
+CREATE TRIGGER protectDefault
+  BEFORE DELETE
+  ON STAGE
+  FOR EACH ROW
+  EXECUTE PROCEDURE protectDefault();
+
+  
+ CREATE FUNCTION protectDefault() RETURNS trigger AS $protectDefault$
+    BEGIN            
+        IF OLD.Stage_Id < 20 THEN
+            RAISE EXCEPTION 'Cannot delete default stage';
+        END IF;
+
+    END;
+$protectDefault$ LANGUAGE plpgsql;
 
 
