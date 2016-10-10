c237f6a985e173f6d58576e23a9357a8854f9f82
Emmanuel Madrigal <emma.madri97@gmail.com>
2016-10-09 21:03:03 -0600

    Basic files for the Mobile App

---
+++ java/construtec/mobile/ProyectWindow.java
@@ -0,0 +1,13 @@
+package construtec.mobile;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ProyectWindow extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_proyect_window);
+    }
+}
---
+++ java/construtec/mobile/RegisterUser.java
@@ -0,0 +1,18 @@
+package construtec.mobile;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class RegisterUser extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_user);
+
+        Intent intent = getIntent();
+    }
+}
