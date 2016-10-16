7858e83fd3e2f98d7add10a442a4d454a03b057f
xxMauricioxx <maumonteroj@gmail.com>
2016-10-15 18:21:56 -0600

    agregue el controller de roles falta agregar la clase de conexion a la DB

---
+++ Controllers/RolesController.cs
@@ -0,0 +1,60 @@
+﻿using ConnectionDB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ConstruTec.Controllers
+{
+    [RoutePrefix("Roles")]
+    public class RolesController : ApiController
+    {
+        [Route("{id}")]
+        [HttpGet]
+        public IHttpActionResult getRoles(long id)
+        {
+            var dato = ConnectionDB.Instance.get_Roles(id);
+            if (dato == null)
+            {
+                return NotFound();
+            }
+            return Ok(dato);
+        }
+
+        [Route("~/getAllRoles")] 
+        [HttpGet]
+        public IHttpActionResult getAllClients()
+        {
+            var dato = ConnectionDB.Instance.get_allRoles();
+            return Ok(dato);
+        }
+
+        [Route("{id}/{campo}/{newValue}")]
+        [HttpPut]
+        public IHttpActionResult PutRoles(long id, string campo, string newValue)
+        {
+            if (campo == "nombre")
+            {
+                ConnectionDB.Instance.update_Nombre_Roles(id, newValue);
+            }
+            return Ok();
+        }
+
+        [HttpPost]
+        public IHttpActionResult postRoles([FromBody] Models.Roles roles )
+        {
+            ConnectionDB.Instance.crear_Roles(roles);
+            return Ok();
+        }
+
+        [Route("{id}")]
+        [HttpDelete]
+        public IHttpActionResult deleteRoles(long id)
+        {
+            ConnectionDB.Instance.eliminar_Roles(id);
+            return Ok();
+        }
+    }
+}
