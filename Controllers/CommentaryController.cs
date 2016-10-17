366987025a9febeb7aa20ca220bdb79bdf53db01
xxMauricioxx <maumonteroj@gmail.com>
2016-10-16 22:19:59 -0600

    Se agrega completamente el controller de commentary

--- Controllers/CommentaryController.cs
+++ Controllers/CommentaryController.cs
@@ -7,7 +7,55 @@ using System.Web.Http;
 
 namespace ConstruTec.Controllers
 {
+    [RoutePrefix("Commentary")]
     public class CommentaryController : ApiController
     {
+        [Route("{id}")]
+        [HttpGet]
+        public IHttpActionResult getCommentary(long id)
+        {
+            var dato = ConnectionDB.Instance.get_Commentary(id);
+            if (dato == null)
+            {
+                return NotFound();
+            }
+            return Ok(dato);
+        }
+
+        [Route("~/getAllCommentary")]
+        [HttpGet]
+        public IHttpActionResult getAllCommentary()
+        {
+            var dato = ConnectionDB.Instance.get_allCommentary();
+            return Ok(dato);
+        }
+
+        [Route("{id}/{campo}/{newValue}")]
+        [HttpPut]
+        public IHttpActionResult PutCommentary(long id, string campo, string newValue)
+        {
+            if (campo == "Commentary")
+            {
+                ConnectionDB.Instance.update_Commentary(id, campo, newValue);
+            }
+            return Ok();
+        }
+
+        [Route("")]
+        [HttpPost]
+        public IHttpActionResult postCommentary([FromBody] Models.Commentary commentary)
+        {
+            ConnectionDB.Instance.crear_Commentary(commentary);
+            return Ok();
+        }
+
+        [Route("{id}")]
+        [HttpDelete]
+        public IHttpActionResult deleteCommentary(long id)
+        {
+            ConnectionDB.Instance.eliminar_Commentary(id);
+            return Ok();
+        }
+
     }
 }
